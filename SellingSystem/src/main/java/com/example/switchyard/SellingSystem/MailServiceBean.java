494a8da022211be1179adb4c39972f35c2bb311e
Nico Fischer <nico.fischer22@googlemail.com>
2012-12-30 15:46:09 +0100

    alles fertig

--- SellingSystem/src/main/java/com/example/switchyard/SellingSystem/MailServiceBean.java
+++ SellingSystem/src/main/java/com/example/switchyard/SellingSystem/MailServiceBean.java
@@ -1,5 +1,14 @@
 package com.example.switchyard.SellingSystem;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.apache.log4j.Logger;
 import org.switchyard.component.bean.Service;
 
@@ -10,7 +19,51 @@ public class MailServiceBean implements MailService {
 
 	@Override
 	public void warnArticleCountIsLow(SellInformation sellInformation) {
-		LOGGER.info("Mail was sent");
+		// Recipient's email ID needs to be mentioned.
+	      String to = "einkauf@127.0.0.1";
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "seller@127.0.0.1";
+
+	      // Assuming you are sending email from localhost
+	      String host = "127.0.0.1";
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("127.0.0.1", host);
+
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(properties);
+
+	      try{
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO,
+	                                  new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject("Count for product: " + sellInformation.getName() + " is under 100!");
+
+	         // Now set the actual message
+	         message.setText("The product count for product " + sellInformation.getName() + " with the id: " + sellInformation.getId() + " is " + sellInformation.getCount() + "!"
+	        		 + "\n\nOrder some more! \n\nGreets your selling system");
+
+	         // Send message
+	         Transport.send(message);
+	         LOGGER.info("Mail was sent");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+		
+		
+		
 		
 	}
 
